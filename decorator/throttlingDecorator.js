d0004db7fa3cf95ac94b74e2cd51e256afb3e0bd
asosunoff <al.sosunoff@gmail.com>
2020-12-23 01:01:16 +0300

    fix

--- decorator/throttlingDecorator.js
+++ decorator/throttlingDecorator.js
@@ -1,23 +1,35 @@
 function throttlingDecorator(func, delay = 0) {
 	let isCooldown = false;
+	let saveThis = null;
+	let saveArgs = null;
+	let IS_CALL = false;
 
-	return function (...arg) {
-		if (isCooldown) return;
+	return function wrapper(...arg) {
+		if (isCooldown) {
+			IS_CALL = true;
+			saveThis = this;
+			saveArgs = arg;
+			return;
+		}
 
-		const result = func.apply(this, arg);
+		func.apply(this, arg);
 
 		isCooldown = true;
 
-        setTimeout(() => (isCooldown = false), delay);
-        
-		return result;
+		setTimeout(() => {
+			isCooldown = false;
+			if (IS_CALL) {
+				wrapper.apply(saveThis, saveArgs);
+				IS_CALL = false;
+			}
+		}, delay);
 	};
 }
 
 /* **TEST** */
 
-function test() {
-	return 1;
+function test(x) {
+	console.log(x);
 }
 
-test = throttlingDecorator(test, 1000);
+test = throttlingDecorator(test, 10000);
